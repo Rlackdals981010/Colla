57120dfc1524f548ffa2b5a6668283109a6e2d20
Changmin Kim <ksjchm4@gmail.com>
2025-05-12 14:55:49 +0900

    [creat] 프로젝트 생성

---
+++ src/main/java/com/dolloer/colla/CollaApplication.java
@@ -0,0 +1,13 @@
+package com.dolloer.colla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dolloer/colla/CollaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolloer.colla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
