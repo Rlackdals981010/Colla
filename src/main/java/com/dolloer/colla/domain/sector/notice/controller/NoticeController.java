145847072e40af59a6c92f7e0f9fdd21259511b9
Changmin Kim <ksjchm4@gmail.com>
2025-05-26 14:50:40 +0900

    [feat] <공지 섹터> 공지 섹터 초기 설정

--- src/main/java/com/dolloer/colla/domain/sector/notice/controller/NoticeController.java
+++ src/main/java/com/dolloer/colla/domain/sector/notice/controller/NoticeController.java
@@ -1,4 +1,12 @@
 package com.dolloer.colla.domain.sector.notice.controller;
 
+import com.dolloer.colla.domain.sector.notice.service.NoticeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class NoticeController {
+
+    private final NoticeService noticeService;
 }
--- src/main/java/com/dolloer/colla/domain/sector/notice/service/NoticeService.java
+++ src/main/java/com/dolloer/colla/domain/sector/notice/service/NoticeService.java
@@ -1,4 +1,12 @@
 package com.dolloer.colla.domain.sector.notice.service;
 
+import com.dolloer.colla.domain.sector.notice.repository.NoticeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class NoticeService {
+
+    private final NoticeRepository noticeRepository;
 }
